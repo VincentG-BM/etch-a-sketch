8a287084ec4ada53020a684e06490780792aac3a
Vincent Biancarelli <Vincentgbm@gmail.com>
2023-10-25 19:12:26 -0400

    Reworked all code and completed etch-a-sketch

--- index.js
+++ index.js
@@ -1,9 +1,61 @@
-const container = document.querySelector('#container');
+const reset = document.querySelector('button');
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
+
+function createGrid(index) {
+    const container = document.querySelector('#container');
+    container.style.gridTemplateColumns = `repeat(${index}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${index}, 1fr)`;
+    for (i = 0; i < index * index; i++) {
         const box = document.createElement('div');
-        box.classList.add('grid');
+        box.classList.add('box');
+        box.addEventListener('pointerover', addRGB)
         container.appendChild(box);
     }
 }
+
+
+createGrid(16);
+
+function addRGB(e) {
+    e.target.style = `background-color:
+    rgb(${Math.floor(Math.random() *
+        256)},${Math.floor(Math.random() *
+            256)},${Math.floor(Math.random() * 256)})`;
+    e.target.removeEventListener('pointerover', addRGB);
+}
+
+
+function resetGrid() {
+    let input = prompt("Please enter a new number for your grid.");
+    if (input === undefined || input === null || input === "") {
+        return;
+    }
+
+    while (input > 100 || input < 1) {
+        input = prompt("Entered number is out of range. Please enter a number between 1 and 100.")
+    }
+
+    const container = document.querySelector('#container');
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+    createGrid(input);
+    let grid = document.querySelectorAll('.grid');
+    grid.forEach(gridBox => {
+        gridBox.addEventListener('pointerover', () => {
+            gridBox.style.backgroundColor = null;
+        })
+    });
+
+};
+
+let gridAmount = 16;
+createGrid(gridAmount);
+let grid = document.querySelectorAll('.grid');
+grid.forEach(gridBox => {
+    gridBox.addEventListener('pointerover', addRGB);
+});
+
+const newGrid = document.querySelector('#new-grid');
+
+newGrid.addEventListener("click", resetGrid);
